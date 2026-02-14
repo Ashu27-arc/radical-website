76b1df8ea5405a341dcfa20fa88f873f7a930785
Ashu27-arc <ashrathor0290@gmail.com>
2026-02-14 15:57:42 +0530

    pk

--- dist/dev/server/chunks/ssr/_next-internal_server_app_study-india_page_actions_e7b9f616.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/study-india/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_study-india_page_actions_e7b9f616.js.map
