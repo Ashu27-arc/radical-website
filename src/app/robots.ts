48d42accac7b3422c290d13e8c55c4dbe62aa9f2
Ashutosh Rathor <111623370+Ashu27-arc@users.noreply.github.com>
2026-04-27 12:41:13 +0530

    ok

---
+++ src/app/robots.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next';
+
+export const revalidate = 3600; // revalidate every hour
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/api/',
+        '/_next/',
+        '/admin/',
+        '/wp-admin/',
+        '/private/',
+      ],
+    },
+    sitemap: 'https://www.radicaleducation.in/sitemap.xml',
+    host: 'https://www.radicaleducation.in',
+  };
+}
